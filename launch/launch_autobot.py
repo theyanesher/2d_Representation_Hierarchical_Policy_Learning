6984730ff10bfdf2018e57c43b9574e50387d97c
yufei <yufeiwang63@gmail.com>
2024-05-16 22:11:00 -0400

    minor

--- launch/launch_autobot.py
+++ launch/launch_autobot.py
@@ -2,6 +2,6 @@ import os
 
 autobot_user_name = "yufeiw2"
 autobot_project_folder = "RoboGen_sim2real"
-os.system("rsync -avrz --exclude='pytorch_sac/exp' --exclude='3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy/data' --exclude='objaverse_utils/data/default_tag_embeddings_*.pt' --exclude='__pycache__' --exclude='experiment' --exclude='.hydra' --exclude='.vscode' --exclude='runs' --exclude='video' --exclude='video_rl_game' --exclude='data/icml*' --exclude='data/partnet' --exclude='data/PartManip_DataRelease.zip' ./ autobot:/project_data/held/{}/{}/".format(
+os.system("rsync -avrz --exclude='pytorch_sac/exp' --exclude='data/dp3_demo' --exclude='3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy/data' --exclude='objaverse_utils/data/default_tag_embeddings_*.pt' --exclude='__pycache__' --exclude='experiment' --exclude='.hydra' --exclude='.vscode' --exclude='runs' --exclude='video' --exclude='video_rl_game' --exclude='data/icml*' --exclude='data/partnet' --exclude='data/PartManip_DataRelease.zip' ./ autobot:/project_data/held/{}/{}/".format(
     autobot_user_name, autobot_project_folder
 ))
--- run.sh
+++ run.sh
@@ -1,29 +1,76 @@
 # python manipulation/old_test_opening_primitve.py
 
 demo_name=0511-vary-obj-loc-ori-init-angle-robot-init-joint-near-handle-300-demo-0.4-0.15-translation-first
-data_name=0512-vary-obj-loc-ori-init-angle-robot-init-joint-near-handle-300-demo-0.4-0.15-translation-first-more-points-longer-horizon-depend
+# data_name=0512-vary-obj-loc-ori-init-angle-robot-init-joint-near-handle-300-demo-0.4-0.15-translation-first-more-points-longer-horizon-depend
+# save_data_name=0513-vary-obj-loc-ori-init-angle-robot-init-joint-near-handle-300-demo-0.4-0.15-translation-first-joint-angle-action
+# save_data_name=0513-vary-obj-loc-ori-init-angle-robot-init-joint-near-handle-300-demo-0.4-0.15-translation-first-with-seg-mask
+save_data_name=0513-vary-obj-loc-ori-init-angle-robot-init-joint-near-handle-300-demo-0.4-0.15-translation-first-only-handle-points
 exp_folder=data/temp/open_the_door_of_the_storagefurniture_by_its_handle_StorageFurniture_41510_2024-03-27-15-59-54/task_open_the_door_of_the_storagefurniture_by_its_handle
 task_beg_idx=0
 task_end_idx=1
-pointcloud_num=8000
+use_joint_angle=0
+use_segmask=0
+only_handle_points=1
+if [ $only_handle_points -eq 1 ]; then
+    echo "only sample handle points"
+    pointcloud_num=2000
+else
+    echo "not only handle points"
+    pointcloud_num=4500
+fi
 
 # python 3d_diffusion_policy/extract_data_from_states_new.py --folder_name data/temp/ --object_name storagefurniture \
-#     --save_path "data/dp3_demo/${data_name}" --exp_name "${demo_name}" \
+#     --save_path "data/dp3_demo/${save_data_name}" --exp_name "${demo_name}" \
 #     --task_beg_idx "${task_beg_idx}" --task_end_idx "${task_end_idx}" \
 #     --pointcloud_num "${pointcloud_num}" \
-#     --use_extracted 0
+#     --use_extracted 1 --use_joint_angle "${use_joint_angle}" --use_segmask "${use_segmask}" \
+#     --only_handle_points "${only_handle_points}" \
+#     --num_experiment 2
 
 cd 3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy
 
-horizon=8
-n_obs_steps=4
-exp_name=0512-vary-obj-loc-ori-init-more-points-longer-horizon-depend
+horizon=4
+n_obs_steps=2
+# exp_name=0512-vary-obj-loc-ori-init-more-points-longer-horizon-depend
+# exp_name=0513-vary-obj-loc-ori-init-joint-angle
+# exp_name=0513-vary-obj-loc-ori-init-segmask
+exp_name=0513-vary-obj-loc-ori-only-handle-points
+if [ $use_joint_angle -eq 1 ]; then
+    echo "use joint angle"
+    action_dim=8
+    agent_pos_dim=9
+else
+    echo "not use joint angle"
+    action_dim=10
+    agent_pos_dim=10
+fi
 
-python train.py --config-name=dp3.yaml task=robogen_open_door exp_name="${exp_name}" eval_first=0  \
-    task.dataset.zarr_path="${PROJECT_DIR}/data/dp3_demo/${data_name}/" \
+if [ $use_segmask -eq 1 ]; then
+    echo "use segmask"
+    pc_channel=5
+elif [ $only_handle_points -eq 1 ]; then
+    echo "use only handle points"
+    pc_channel=5
+else
+    echo "not use segmask"
+    pc_channel=3
+fi
+
+python train.py --config-name=dp3.yaml task=robogen_open_door exp_name="${exp_name}" eval_first=1  \
+    task.dataset.zarr_path="${PROJECT_DIR}/data/dp3_demo/${save_data_name}/" \
     task.env_runner.experiment_name="${demo_name}" \
     task.env_runner.experiment_folder="${exp_folder}" \
-    horizon="${horizon}" n_obs_steps="${n_obs_steps}" 
+    task.env_runner.num_point_in_pc="${pointcloud_num}" \
+    task.env_runner.use_joint_angle="${use_joint_angle}" \
+    task.env_runner.use_segmask="${use_segmask}" \
+    task.env_runner.only_handle_points="${only_handle_points}" \
+    horizon="${horizon}" n_obs_steps="${n_obs_steps}" \
+    task.shape_meta.obs.agent_pos.shape="[${agent_pos_dim}]" \
+    task.shape_meta.action.shape="[${action_dim}]" \
+    policy.pointcloud_encoder_cfg.in_channels="${pc_channel}" 
+
+
+
 # python eval_robogen_new.py --config-name=dp3.yaml task=robogen_open_door exp_name=eval
 # python eval_robogen_new.py --config-name=dp3.yaml task=robogen_open_door exp_name=debug
 # singularity shell --bind /project_data/held/yufeiw2/RoboGen_sim2real/:/mnt/RoboGen_sim2real/ --nv /project_data/held/yufeiw2/robogen-dp3.sif
