ed2dbcb7e332f9af85926109b8596b0cfaa38184
yufeiwang63 <yufeiwang63@gmail.com>
2024-06-30 17:42:08 -0400

    update scripts

--- run-act3d-ddp.sh
+++ run-act3d-ddp.sh
@@ -23,8 +23,8 @@ opened_threshold=0.4
 
 # observation_mode=act3d
 # observation_mode=act3d_goal
-observation_mode=act3d_displacement_gripper_to_object
-# observation_mode=act3d_goal_displacement_gripper_to_object
+# observation_mode=act3d_displacement_gripper_to_object
+observation_mode=act3d_goal_displacement_gripper_to_object
 pointcloud_num=4500
 
 # python 3d_diffusion_policy/filter_simulation_error.py --folder_name data/temp/ --object_name storagefurniture     --save_path "data/dp3_demo/${save_data_name}" --exp_name "${demo_name}"     --task_beg_idx "${task_beg_idx}" --task_end_idx "${task_end_idx}"     --pointcloud_num "${pointcloud_num}"     --use_extracted 0     --num_experiment 1000     --observation_mode "${observation_mode}"     --parallel 0     --opened_threshold "${opened_threshold}" --demo_folder /project_data/held/yufeiw2/RoboGen_sim2real/data/dp3_demo/0531-act3d-obj-46462
@@ -45,18 +45,21 @@ cd 3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy
 # save_data_name_0=0527-act3d-always-close
 # save_data_name_0=0527-act3d-always-close-with-goal
 # save_data_name_0=0607-act3d-obj-41510-remove-reaching-collision-resize-2
+# save_data_name_0=0616-act3d-obj-41510-remove-reaching-collision-resize-2-per-step
 save_data_name_0=0616-act3d-obj-41510-remove-reaching-collision-resize-2-per-step
+save_data_name_0=0626-act3d-obj-41510-per-step-combine-2-action-gripper-goal-displacement-to-closest-obj-point-filtered-zero-closing-action
 
 # save_data_name_1=0531-act3d-obj-45448
 # save_data_name_1=0607-act3d-obj-45448-remove-reaching-collision-resize-2-full
 # save_data_name_1=0616-act3d-obj-45448-remove-reaching-collision-resize-2-full-per-step
 # save_data_name_1=0616-act3d-obj-45448-remove-reaching-collision-resize-2-full-per-step-gripper-goal
-# save_data_name_1=0622-act3d-obj-45448-remove-reaching-collision-resize-2-full-per-step-gripper-goal-displacement-to-closest-obj-point
-save_data_name_1=0623-act3d-obj-45448-reach-to-contact-smoothed-per-step-combine-2-action-gripper-goal-displacement-to-closest-obj-point
+save_data_name_1=0622-act3d-obj-45448-remove-reaching-collision-resize-2-full-per-step-gripper-goal-displacement-to-closest-obj-point
+# save_data_name_1=0623-act3d-obj-45448-reach-to-contact-smoothed-per-step-combine-2-action-gripper-goal-displacement-to-closest-obj-point
 
 # save_data_name_2=0531-act3d-obj-46462
 # save_data_name_2=0607-act3d-obj-46462-remove-reaching-collision-resize-2
-save_data_name_2=0616-act3d-obj-46462-remove-reaching-collision-resize-2-per-step
+# save_data_name_2=0616-act3d-obj-46462-remove-reaching-collision-resize-2-per-step
+save_data_name_2=0624-act3d-obj-46462-per-step-combine-2-action-gripper-goal-displacement-to-closest-obj-point-filtered-zero-closing-action
 
 demo_name_0=0511-vary-obj-loc-ori-init-angle-robot-init-joint-near-handle-300-demo-0.4-0.15-translation-first
 demo_name_1=0511-vary-obj-2-loc-ori-init-angle-robot-init-joint-near-handle-300-demo-0.4-0.15-translation-first
@@ -79,6 +82,11 @@ exp_name="0622-per-step-load-ddp-obj-45448-horizon-${horizon}-train-episodes-${n
 # exp_name="0622-per-step-load-ddp-obj-45448-horizon-${horizon}-train-episodes-${num_load_episodes}-with-gripper-displacement-to-closest-obj-point"
 exp_name="0623-smoothed-obj-45448-horizon-${horizon}-train-episodes-${num_load_episodes}-gripper-goal-with-gripper-displacement-to-closest-obj-point"
 exp_name="0623-smoothed-obj-45448-horizon-${horizon}-train-episodes-${num_load_episodes}-with-gripper-displacement-to-closest-obj-point"
+exp_name="0624-per-step-load-ddp-obj-45448-horizon-${horizon}-train-episodes-${num_load_episodes}-with-gripper-displacement-to-closest-obj-point-self-attention"
+exp_name="0624-ddp-obj-45448-hor-${horizon}-train-ep-${num_load_episodes}-gripper-goal-w-gripper-displacement-to-closest-objpoint-self-attention"
+exp_name="0625-ddp-obj-45448-46462-hor-${horizon}-train-ep-${num_load_episodes}-gripper-goal-w-gripper-displacement-to-closest-objpoint"
+exp_name="0627-ddp-obj-45448-46462-41510-hor-${horizon}-train-ep-${num_load_episodes}-gripper-goal-w-gripper-displacement-to-closest-objpoint-no-self-attention"
+exp_name="0629-ddp-obj-45448-46462-41510-hor-${horizon}-train-ep-${num_load_episodes}-w-gripper-displacement-to-closest-objpoint"
 
 action_dim=10
 agent_pos_dim=10
@@ -86,10 +94,10 @@ pc_channel=3
 
 torchrun --standalone --nproc_per_node=4 \
     train_ddp.py --config-name=dp3.yaml task=robogen_open_door exp_name="${exp_name}" eval_first=0  \
-    task.dataset.zarr_path="[/scratch/yufei/dp3_demo/${save_data_name_1}]" \
-    task.env_runner.demo_experiment_path="[/scratch/yufei/dp3_demo/${save_data_name_1}]" \
-    task.env_runner.experiment_name="[${demo_name_1}]" \
-    task.env_runner.experiment_folder="[${exp_folder_1}]" \
+    task.dataset.zarr_path="[/scratch/yufei/dp3_demo/${save_data_name_0}, /scratch/yufei/dp3_demo/${save_data_name_1}, /scratch/yufei/dp3_demo/${save_data_name_2}]" \
+    task.env_runner.demo_experiment_path="[/scratch/yufei/dp3_demo/${save_data_name_0}, /scratch/yufei/dp3_demo/${save_data_name_1}, /scratch/yufei/dp3_demo/${save_data_name_2}]" \
+    task.env_runner.experiment_name="[${demo_name_0}, ${demo_name_1}, ${demo_name_2}]" \
+    task.env_runner.experiment_folder="[${exp_folder_0}, ${exp_folder_1}, ${exp_folder_2}]" \
     task.env_runner.num_point_in_pc="${pointcloud_num}" \
     task.env_runner.use_joint_angle="${use_joint_angle}" \
     task.env_runner.use_segmask="${use_segmask}" \
@@ -114,7 +122,9 @@ torchrun --standalone --nproc_per_node=4 \
     dataloader.batch_size=30 \
     val_dataloader.batch_size=30 \
     policy.act3d_encoder_cfg.mode=keep_position_feature_in_attention_feature_with_gripper_displacement_to_closest_object \
+    policy.act3d_encoder_cfg.self_attention=true \
     # policy.act3d_encoder_cfg.goal_mode=cross_attention_to_goal \
+    # policy.act3d_encoder_cfg.goal_mode=cross_attention_to_goal_not_concat_and_self_attention \
     # policy.act3d_encoder_cfg.mode=keep_position_feature_in_attention_feature \
 
 # python eval_robogen_new.py --config-name=dp3.yaml task=robogen_open_door exp_name=eval
