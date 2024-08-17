d396a4d5be808ff4f11ba745240d51cd61ca2525
yufei <yufeiwang63@gmail.com>
2024-08-17 17:19:41 -0400

    minor

--- prepare.sh
+++ prepare.sh
@@ -1,11 +1,11 @@
 # bin/sh
 
-export PATH=/opt/conda/bin:$PATH
-source /opt/conda/etc/profile.d/conda.sh
-conda activate unisim
-# export YUFEI_OPENAI_API_KEY="xxx" # TODO: embed this in singularity
-export PYTHONPATH=${PWD}:$PYTHONPATH
-export PYTHONPATH=${PWD}/rl_games:$PYTHONPATH
+# export PATH=/opt/conda/bin:$PATH
+# source /opt/conda/etc/profile.d/conda.sh
+# conda activate unisim
+# # export YUFEI_OPENAI_API_KEY="xxx" # TODO: embed this in singularity
+# export PYTHONPATH=${PWD}:$PYTHONPATH
+# export PYTHONPATH=${PWD}/rl_games:$PYTHONPATH
 
 export PYTHONPATH=${PWD}:$PYTHONPATH
 export PYTHONPATH=${PWD}/3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy:$PYTHONPATH
@@ -13,3 +13,4 @@ export PROJECT_DIR=${PWD}
 export NUMEXPR_MAX_THREADS=90
 export HYDRA_FULL_ERROR=1
 export YUFEI_OPENAI_API_KEY=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
+# sed -i 's/\r$//' prepare.sh
