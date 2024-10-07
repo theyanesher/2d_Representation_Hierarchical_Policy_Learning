822c7790670ff7ede369bae2a4e759e851844668
yufeiwang63 <yufeiwang63@gmail.com>
2024-10-06 21:52:23 -0400

    add weighted displacement scripts

--- 3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy/eval_robogen_with_goal_PointNet.py
+++ 3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy/eval_robogen_with_goal_PointNet.py
@@ -22,7 +22,6 @@ from multiprocessing import Pool
 import time
 import yaml
 import pickle as pkl
-from test_PointNet2.model import PointNet2_small2, PointNet2, PointNet2_super
 
 def construct_env(cfg, config_file, solution_path, task_name, init_state_file):
     env, _ = build_up_env(
@@ -391,6 +390,7 @@ if __name__ == "__main__":
     parser.add_argument("--random_object_translation", type=bool, default=False)
     parser.add_argument("--use_predicted_goal", type=bool, default=True)
     parser.add_argument("--test_cross_category", type=bool, default=False)
+    parser.add_argument("--model_invariant", type=bool, default=False)
     args = parser.parse_args()
     
     num_worker = 30
@@ -400,24 +400,27 @@ if __name__ == "__main__":
         # best 50 objects
         exp_dir = "/project_data/held/chialiak/RoboGen-sim2real/3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy/data/07201526-act3d_goal_mlp-horizon-8-num_load_episodes-1000/2024.07.20/15.26.54_train_dp3_robogen_open_door"
         checkpoint_name = 'latest.ckpt'
+    else:
+        exp_dir = args.low_level_exp_dir
+        checkpoint_name = args.low_level_ckpt_name
 
-        with hydra.initialize(config_path='diffusion_policy_3d/config'):  # same config_path as used by @hydra.main
-            recomposed_config = hydra.compose(
-                config_name="dp3.yaml",  # same config_name as used by @hydra.main
-                overrides=OmegaConf.load("{}/.hydra/overrides.yaml".format(exp_dir)),
-            )
-        cfg = recomposed_config
-        
-        workspace = TrainDP3Workspace(cfg)
-        checkpoint_dir = "{}/checkpoints/{}".format(exp_dir, checkpoint_name)
-        workspace.load_checkpoint(path=checkpoint_dir)
+    with hydra.initialize(config_path='diffusion_policy_3d/config'):  # same config_path as used by @hydra.main
+        recomposed_config = hydra.compose(
+            config_name="dp3.yaml",  # same config_name as used by @hydra.main
+            overrides=OmegaConf.load("{}/.hydra/overrides.yaml".format(exp_dir)),
+        )
+    cfg = recomposed_config
+    
+    workspace = TrainDP3Workspace(cfg)
+    checkpoint_dir = "{}/checkpoints/{}".format(exp_dir, checkpoint_name)
+    workspace.load_checkpoint(path=checkpoint_dir)
 
-        policy = deepcopy(workspace.model)
-        if workspace.cfg.training.use_ema:
-            policy = deepcopy(workspace.ema_model)
-        policy.eval()
-        policy.reset()
-        policy = policy.to('cuda')
+    policy = deepcopy(workspace.model)
+    if workspace.cfg.training.use_ema:
+        policy = deepcopy(workspace.ema_model)
+    policy.eval()
+    policy.reset()
+    policy = policy.to('cuda')
     
     if not args.test_cross_category:
         cfg.task.env_runner.experiment_name = ['0705-diverse-objects-vary-obj-loc-ori-init-angle-robot-init-joint-near-handle-300-demo-0.4-0.15-translation-first' for _ in range(10)]
@@ -449,10 +452,23 @@ if __name__ == "__main__":
     
     load_model_path = args.high_level_ckpt_name
         
-    if args.pointnet_class == "PointNet2_small2":
-        pointnet2_model = PointNet2_small2(num_classes=13).to('cuda')
-    elif args.pointnet_class == "PointNet2":
-        pointnet2_model = PointNet2(num_classes=13).to('cuda')
+        
+    if not args.model_invariant:
+        from test_PointNet2.model import PointNet2_small2, PointNet2, PointNet2_super
+        if args.pointnet_class == "PointNet2_small2":
+            pointnet2_model = PointNet2_small2(num_classes=13).to('cuda')
+        elif args.pointnet_class == "PointNet2":
+            pointnet2_model = PointNet2(num_classes=13).to('cuda')
+        elif args.pointnet_class == "PointNet2_super":
+            pointnet2_model = PointNet2_super(num_classes=13).to("cuda")
+            
+    else:
+        from test_PointNet2.model_invariant import PointNet2, PointNet2_super
+        if args.pointnet_class == 'PointNet2':
+            pointnet2_model = PointNet2(num_classes=13).to('cuda')
+        elif args.pointnet_class == 'PointNet2_super':
+            pointnet2_model = PointNet2_super(num_classes=13).to("cuda")
+        
         
     pointnet2_model.load_state_dict(torch.load(load_model_path))
     pointnet2_model.eval()
