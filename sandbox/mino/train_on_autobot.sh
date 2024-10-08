c4598ac2380241ef8980b722767d99151e412895
Mino Nakura <minorunakura@gmail.com>
2024-10-08 13:50:00 -0400

    wot

--- sandbox/mino/train_on_autobot.sh
+++ sandbox/mino/train_on_autobot.sh
@@ -1,7 +1,7 @@
 source /mnt/RoboGen_sim2real/sandbox/mino/init_singularity.sh
 source /mnt/RoboGen_sim2real/prepare.sh
 
-cd 3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy
+cd /mnt/RoboGen_sim2real/3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy
 
 # run training script
 torchrun --standalone --nproc_per_node=4 train_ddp.py --config-name=train_low_level_200_objects.yaml
