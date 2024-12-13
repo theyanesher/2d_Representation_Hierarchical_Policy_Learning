648332d7d24d354a2d72c9614ab9221ff23bf626
yufeiwang63 <yufeiwang63@gmail.com>
2024-12-13 01:30:09 -0500

    minor

--- scripts/datasets/partition_randomize_dataset.py
+++ scripts/datasets/partition_randomize_dataset.py
@@ -8,17 +8,25 @@ all_objs = sorted(all_objs)
 non_randomize_objs_dict = {}
 randomize_objs_dict = {}
 
-for obj in all_objs:
-    if obj.startswith("062") or obj.startswith("0705") or obj.startswith("0712") or obj.startswith("0725") or obj.startswith("0730"):
-        if 'act3d' in obj:
-            obj_id = obj.split("-")[3]
-        else:
-            obj_id = obj.split("-")[2]
-        non_randomize_objs_dict[obj_id] = obj
+# for obj in all_objs:
+#     if obj.startswith("062") or obj.startswith("0705") or obj.startswith("0712") or obj.startswith("0725") or obj.startswith("0730"):
+#         if 'act3d' in obj:
+#             obj_id = obj.split("-")[3]
+#         else:
+#             obj_id = obj.split("-")[2]
+#         non_randomize_objs_dict[obj_id] = obj
 #     elif obj.startswith("0822") or obj.startswith("0815") or obj.startswith("0826"):
 #         obj_id = obj.split("-")[2]
 #         randomize_objs_dict[obj_id] = obj
 #         randomize_objs.append(obj_id)
+
+for idx in range(247):
+    obj =  globals()[f"save_data_name_{idx}"]
+    if 'act3d' in obj:
+        obj_id = obj.split("-")[3]
+    else:
+        obj_id = obj.split("-")[2]
+    non_randomize_objs_dict[obj_id] = obj
 
 
 randomize_obj_dirs = []
@@ -36,20 +44,20 @@ all_randomize_obj_ids = sorted(list(randomize_objs_dict.keys()))
 #     if obj_id not in non_randomize_objs_dict: 
 #         continue # print("************************ not in: ", obj_id)
 #     else: 
-#         print(f"new_partition_save_data_name_{idx}='{non_randomize_objs_dict[obj_id]}'")
+#         print(f"camera_random_200_save_data_name_{idx}='{non_randomize_objs_dict[obj_id]}'")
 #         idx += 1
         
 # for idx2 in range(246):
 #     old_save_data_name = globals()[f"save_data_name_{idx2}"]
 #     obj_id = old_save_data_name.split("-")[-1]
 #     if obj_id not in all_randomize_obj_ids:
-#         print(f"new_partition_save_data_name_{idx}='{old_save_data_name}'")
+#         print(f"camera_random_200_save_data_name_{idx}='{old_save_data_name}'")
 #         idx += 1
 #     if idx == 200:
 #         break
         
 # for obj_dir in randomize_obj_dirs:
-#     print(f"new_partition_save_data_name_{idx}='{obj_dir}'")
+#     print(f"camera_random_200_save_data_name_{idx}='{obj_dir}'")
 #     idx += 1
     
     
@@ -59,20 +67,20 @@ all_randomize_obj_ids = sorted(list(randomize_objs_dict.keys()))
 #     if obj_id not in non_randomize_objs_dict: 
 #         continue # print("************************ not in: ", obj_id)
 #     else: 
-#         print(f"new_partition_save_data_name_{idx}='{non_randomize_objs_dict[obj_id]}'")
+#         print(f"camera_random_100_save_data_name_{idx}='{non_randomize_objs_dict[obj_id]}'")
 #         idx += 1
         
 # for idx2 in range(246):
 #     old_save_data_name = globals()[f"save_data_name_{idx2}"]
 #     obj_id = old_save_data_name.split("-")[-1]
 #     if obj_id not in all_randomize_obj_ids:
-#         print(f"new_partition_save_data_name_{idx}='{old_save_data_name}'")
+#         print(f"camera_random_100_save_data_name_{idx}='{old_save_data_name}'")
 #         idx += 1
 #     if idx == 100:
 #         break
         
 # for obj_id in all_randomize_obj_ids[:75]:
-#     print(f"new_partition_save_data_name_{idx}='{randomize_objs_dict[obj_id]}'")
+#     print(f"camera_random_100_save_data_name_{idx}='{randomize_objs_dict[obj_id]}'")
 #     idx += 1
     
     
@@ -82,20 +90,20 @@ for obj_id in all_randomize_obj_ids[:37]:
     if obj_id not in non_randomize_objs_dict: 
         continue # print("************************ not in: ", obj_id)
     else: 
-        print(f"new_partition_save_data_name_{idx}='{non_randomize_objs_dict[obj_id]}'")
+        print(f"camera_random_50_save_data_name_{idx}='{non_randomize_objs_dict[obj_id]}'")
         idx += 1
         
 for idx2 in range(246):
     old_save_data_name = globals()[f"save_data_name_{idx2}"]
     obj_id = old_save_data_name.split("-")[-1]
     if obj_id not in all_randomize_obj_ids:
-        print(f"new_partition_save_data_name_{idx}='{old_save_data_name}'")
+        print(f"camera_random_50_save_data_name_{idx}='{old_save_data_name}'")
         idx += 1
     if idx == 50:
         break
         
 for obj_id in all_randomize_obj_ids[:37]:
-    print(f"new_partition_save_data_name_{idx}='{randomize_objs_dict[obj_id]}'")
+    print(f"camera_random_50_save_data_name_{idx}='{randomize_objs_dict[obj_id]}'")
     idx += 1
     
 
