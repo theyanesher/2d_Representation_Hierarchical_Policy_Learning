940435260b81ca58a6bd78d740d0f6bb4cc2978b
Mino Nakura <minorunakura@gmail.com>
2024-12-31 17:54:45 -0500

    fixed

--- 3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy/diffusion_policy_3d/dataset/robogen_dataset.py
+++ 3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy/diffusion_policy_3d/dataset/robogen_dataset.py
@@ -42,8 +42,8 @@ def get_zarry_paths(zarr_path):
         all_zarr_paths = all_zarr_paths_part_1 + all_zarr_paths_part_2
     
     # dataset_prefix = '/home/mino/Software/RoboGen-sim2real/data/dp3_demo_combined_2_step_0'
-    # dataset_prefix = '/scratch/yufeiw2/dp3_demo_combined_2_step_0'
-    dataset_prefix = '/local/'
+    dataset_prefix = '/scratch/yufeiw2/dp3_demo_combined_2_step_0'
+    # dataset_prefix = '/local/'
     
     if zarr_path == '10_object_low_level':
         all_zarr_paths = ["{}/{}".format(dataset_prefix, globals()["save_data_name_{}".format(i)]) for i in range(10)]
