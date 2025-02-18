7d12bacbc8fdf2968822afae9217dd8bb99b9ae4
Pratik Bhowal <pbhowal@autobot-0-25.eth>
2025-02-18 13:58:55 -0500

    Minor Fix

--- 3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy/diffusion_policy_3d/common/gripper_orientation_from_4_points.py
+++ 3d_diffusion_policy/3D-Diffusion-Policy/3D-Diffusion-Policy/diffusion_policy_3d/common/gripper_orientation_from_4_points.py
@@ -1,9 +1,9 @@
-import pybullet as p
+#import pybullet as p
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 import torch
 from manipulation.infer_utils import matrix_to_rotation_6d_numpy, rotation_6d_to_matrix
-
+from manipulation.utils import rotation_transfer_matrix_to_6D
 def compute_plane_normal(gripper_pcd):
     x1 = gripper_pcd[0]
     x2 = gripper_pcd[1]
@@ -57,8 +57,13 @@ def _get_gripper_pos_orient_from_4_points(original_gripper_pcd, gripper_pcd, ori
     gripper_pos = original_gripper_pos + gripper_pcd[3] - original_gripper_pcd[3]
     original_R = quaternion_to_rotation_matrix(original_gripper_orn)
     R = np.expand_dims(np.dot(R, original_R), axis = 0)
-    #print("RRRRRRRRRR", R.shape)
-    gripper_orn = matrix_to_rotation_6d_numpy(R)
+    #print("RRRRRRRRRR", R)
+    gripper_orn = matrix_to_rotation_6d_numpy(R.swapaxes(1, 2))
+    #print("Gripper Orn Initial", gripper_orn)
+    #gripper_orn = matrix_to_rotation_6d_numpy(R)
+    #gripper_orn = rotation_transfer_matrix_to_6D(R).reshape(1,-1)
+    #print("Gripper Orn Initial", gripper_orn_alternate)
+    #print("Gripper Orn Alternate", gripper_orn)
     #print("HELOOOOOOOOOOOOOOOOOOO", gripper_orn.shape)
     return gripper_pos, np.squeeze(gripper_orn, axis = 0)
 
@@ -94,10 +99,28 @@ def get_points_from_pos_rotation_matrix(pos, orient):
        [ 0.08953098,  0.0484529 ,  0.80711854],
        [ 0.11198021,  0.00245327,  0.7828771 ]])
     original_gripper_orn = np.array([0.97841681, 0.19802945, 0.0581003 , 0.01045192])
-    absolute_rotation = rotation_6d_to_matrix(orient)
+    from manipulation.utils import rotation_transfer_6D_to_matrix
+    absolute_rotation = rotation_transfer_6D_to_matrix(orient.numpy())
+    #print("absolute_rotation", absolute_rotation)
+    #import pdb; pdb.set_trace();
+    #absolute_rotation_2 = rotation_6d_to_matrix(orient)
+    #print("absolute_rotation_2", absolute_rotation_2)
+    #print("absolute_rotation_2", absolute_rotation_2 - absolute_rotation)
     original_R = quaternion_to_rotation_matrix(original_gripper_orn)
     rotation_transfer = absolute_rotation * original_R.T
     original_pcd = original_gripper_pcd - original_gripper_pcd[3]
     rotated_pcd = np.dot(original_pcd, rotation_transfer.T)
     gripper_pcd = rotated_pcd + pos
     return gripper_pcd
+
+
+
+gripper_pcd = np.array([[ 0.7666899, -0.16390935, 0.44825187],
+    [ 0.8285312, -0.16042456, 0.4600023 ],
+    [ 0.7625436, -0.1860767, 0.38937932],
+    [ 0.8155203, -0.17972143, 0.40836987]])
+gripper_10d = get_gripper_pos_orient_from_4_points_torch(gripper_pcd)
+print(gripper_10d)
+gripper_pcd_reconstructed = get_points_from_pos_rotation_matrix(gripper_10d[:3], torch.tensor(gripper_10d[3:]))
+print(gripper_pcd_reconstructed)
+
