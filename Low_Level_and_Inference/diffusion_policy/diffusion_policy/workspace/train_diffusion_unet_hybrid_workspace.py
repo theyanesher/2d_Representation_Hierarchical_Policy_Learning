fd79e3aa6cfcdd8f9aa365d5d2772eaa456d5c68
prat1999 <pratikbhowal1999@gmail.com>
2026-03-28 01:19:49 -0400

    Minor Chnages

--- Low_Level_and_Inference/diffusion_policy/diffusion_policy/workspace/train_diffusion_unet_hybrid_workspace.py
+++ Low_Level_and_Inference/diffusion_policy/diffusion_policy/workspace/train_diffusion_unet_hybrid_workspace.py
@@ -155,6 +155,7 @@ class TrainDiffusionUnetHybridWorkspace(BaseWorkspace):
         # training loop
         log_path = os.path.join(self.output_dir, 'logs.json.txt')
         with JsonLogger(log_path) as json_logger:
+            print("TOTAL EPOCHSSSSSSSSSSSS", cfg.training.num_epochs)
             for local_epoch_idx in range(cfg.training.num_epochs):
                 step_log = dict()
                 # ========= train for this epoch ==========
